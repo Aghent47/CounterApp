2561c4d712b355a3df9130daa89a0ef2c8a5eca4
Aghent47 <neiderkk@gmail.com>
2023-04-19 13:54:08 -0500

    StyleSheet

--- src/screens/Counter.tsx
+++ src/screens/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 export const Counter = () => {
 
     const [contador, setContador] = useState(0);
@@ -9,28 +9,46 @@ export const Counter = () => {
     };
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{
-                fontSize: 45,
-                fontWeight: 'bold',
-                color: 'white',
-                top: -30,
-            }} >Counter: {contador} </Text>
+        <View style={styles.container}>
+            <Text style={styles.title} >
+                Counter: {contador} </Text>
             <TouchableOpacity onPress={handlePress}>
-                <View style={{
-                    backgroundColor: '#5856D6',
-                    width: 100,
-                    height: 50,
-                    borderRadius: 100,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    top: 30,
-                }} >
-                    <Text>+1</Text>
+                <View style={styles.buttom}  >
+                    <Text style={styles.contentButtom} >+1</Text>
                 </View>
-
             </TouchableOpacity>
         </View>
 
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        color: 'white',
+        top: -30,
+    },
+    buttom: {
+        backgroundColor: '#5856D6',
+        width: 100,
+        height: 50,
+        fontWeight: 'bold',
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 30,
+    },
+    contentButtom: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+},
+
+);
